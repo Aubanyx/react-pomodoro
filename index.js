f38ef38ed38076fb066443ae06f1827c06f8279a
Auban <aubanlabie@gmail.com>
2021-01-27 12:11:11 +0100

    test

---
+++ index.js
@@ -0,0 +1,21 @@
+console.log("salut");
+
+import React from "react";
+import { render } from "react-dom";
+class HelloMessage extends React.Component {
+    render() {
+        return (
+            <div>
+                Hello {this.props.name}
+            </div>
+        );
+    }
+}
+
+render(
+    <HelloMessage name="Taylor" />,
+    document.getElementById('hello-example')
+);
+
+
+console.log("yo");
