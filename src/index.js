602b41ea3aac2a42117179ae85a4b64358fbd79c
Auban <aubanlabie@gmail.com>
2021-01-28 11:05:04 +0100

    modif

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import {render} from "react-dom";
 class HelloMessage extends React.Component {
     render() {
-        return <div>{"Hello"}</div>;
+        return <div>{`Hello ${this.props.name}`}</div>;
     }
 }
 
